0f81bf1a6c2c0a8bfcc53936b05b5ea76f0ecab9
NeeluMadan <39066046+NeeluMadan@users.noreply.github.com>
2019-05-04 18:01:30 +0200

    update for git add

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 
 git checkout master
 git add rosenbrock.png
-git remote add origin2 https://1f0bf4f8a827fd032d6ded4a30d06f5b57ac1f1b@github.com/NeeluMadan/optimization-benchmark.git
+git remote add origin2 https://${GITHUB_TOKEN}@github.com/NeeluMadan/optimization-benchmark.git
 git commit -m "update leaderboard"
 git push origin2
