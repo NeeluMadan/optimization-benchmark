8176304ee3407ad7fff3cca6d2cdc14fcc2478e7
NeeluMadan <39066046+NeeluMadan@users.noreply.github.com>
2019-05-04 17:48:29 +0200

    updated README

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git checkout master
-git remote
 git add rosenbrock.png
-git commit -m "Travis Commit"
-git push 
+git remote add origin2 https://1f0bf4f8a827fd032d6ded4a30d06f5b57ac1f1b@github.com/NiMlr/optimization-benchmark.git
+git commit -m "update leaderboard"
+git push origin2
