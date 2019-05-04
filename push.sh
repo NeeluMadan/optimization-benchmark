acdc59ec5e6ae60b57ae7045a675f6d6ba77b840
NeeluMadan <39066046+NeeluMadan@users.noreply.github.com>
2019-05-04 17:33:38 +0200

    updated README

--- push.sh
+++ push.sh
@@ -3,8 +3,6 @@
 git clone https://github.com/NeeluMadan/optimization-benchmark.git
 cd optimization-benchmark
 git remote
-git config user.email 'er.neelu.m@gmail.com'
-git config user.name NeeluMadan
-git add .
+git add rosenbrock.png
 git commit -m "Travis Commit"
-git push "https://1f0bf4f8a827fd032d6ded4a30d06f5b57ac1f1b@github.com/NeeluMadan/optimization-benchmark.git" master > /dev/null 2>&1
+git push 
